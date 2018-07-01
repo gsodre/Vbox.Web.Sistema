79904dcf093fddf9eac78378140fdd37d9414bd0
Gabriel Sodré <gabrieldeavilasodre@gmail.com>
2018-07-01 20:30:11 -0300

    Primeiro.

---
+++ Vbox.Web.Sistema/wwwroot/js/pvr_form_input_mask.js
@@ -0,0 +1,1 @@
+"use strict";var inputmask = function () {"use strict";$("input").inputmask();};var Mask = function () {"use strict";return {init: function () {inputmask();}}}();$(function () {Mask.init();});
